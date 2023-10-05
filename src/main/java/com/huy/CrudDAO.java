8a6ab4d390d8cf9b8aa0da046d9ac44b6bf1664a
NguyenHuynhGiaHuy <nguyenhuyk77@gmail.com>
2023-10-05 21:56:08 +0700

    lab3

---
+++ src/main/java/com/huy/CrudDAO.java
@@ -0,0 +1,12 @@
+package com.huy;
+
+import java.util.List;
+
+public interface CrudDAO<T> {
+    public boolean add(T t);
+    public T get(String id);
+    public List<T> getAll();
+    public boolean remove(String id);
+    public boolean remove(T t);
+    public boolean update(T t);
+}
